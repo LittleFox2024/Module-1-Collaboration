dc3bd2c24000f0a151f80b370952e8cdcdc253fd
Braden DeForest <btdefores@outlook.com>
2025-06-30 12:00:42 -0400

    Retried django bcause apparently it didn't work right the first time?? Idk...

--- Module 4/django/django/mysite/settings.py
+++ Module 4/django/django/mysite/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Indiana/Indianapolis"
 
 USE_I18N = True
 
@@ -116,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'static'
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
